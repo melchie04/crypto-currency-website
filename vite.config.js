845700c9b590d1793b4b456dc66c5799daeba22b
Melchor Callos <melchorcallos04@gmail.com>
2023-03-21 13:37:30 +0800

    Fix: CORS policy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/crypto-website",
   plugins: [react()],
+  server: {
+    middleware: [
+      createProxyMiddleware('/api', {
+        target: 'https://api.coingecko.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
+        },
+      }),
+    ],
+  },
 })
